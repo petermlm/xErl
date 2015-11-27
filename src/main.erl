44c4c3e471c55697035601b338b99efe3d3c13e0
petermlm <pedromelgueira@gmail.com>
2015-11-27 16:15:22 +0000

    Removed debug function

--- src/main.erl
+++ src/main.erl
@@ -2,8 +2,6 @@
 -author("Pedro Melgueira").
 -compile(export_all).
 
-pexe([A]) -> io:fwrite("~p~n", [A]).
-
 main([Input]) ->
     % Get the program's source
     {ok, Device} = try file:open(Input, [read])
